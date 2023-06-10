0adef9623e246330811cab68ca7204dc40f50951
Pawel <pepawel@users.noreply.github.com>
2023-06-10 19:56:46 +0200

    Fix a bug caused by refactoring

--- lib/crypt_reboot/passphrase_asker.rb
+++ lib/crypt_reboot/passphrase_asker.rb
@@ -3,15 +3,13 @@
 require 'io/console'
 
 module CryptReboot
-  module CryptTab
-    # Ask user for passphrase and return it
-    class PassphraseAsker
-      def call(prompt)
-        print prompt
-        $stdin.noecho(&:gets).chomp
-      ensure
-        puts
-      end
+  # Ask user for passphrase and return it
+  class PassphraseAsker
+    def call(prompt)
+      print prompt
+      $stdin.noecho(&:gets).chomp
+    ensure
+      puts
     end
   end
 end
