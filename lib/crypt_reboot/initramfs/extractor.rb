100ad33694ceb270fb216475a643d06d91b22974
Pawel <pepawel@users.noreply.github.com>
2023-06-15 20:43:18 +0200

    Use better runner, handle exception in Extractor

--- lib/crypt_reboot/initramfs/extractor.rb
+++ lib/crypt_reboot/initramfs/extractor.rb
@@ -6,23 +6,33 @@ module CryptReboot
   module Initramfs
     # Create temporary directory, extract initramfs there and yield, cleaning afterwards
     class Extractor
+      Error = Class.new StandardError
+
       def call(filename)
         tmp_maker.call do |dir|
-          runner.call(tool, filename, dir)
+          run(tool, filename, dir)
           yield dir
         end
       end
 
       private
 
-      attr_reader :tool, :tmp_maker, :runner
+      def run(*args)
+        runner.call(*args)
+      rescue exception_class => e
+        raise Error, cause: e
+      end
+
+      attr_reader :tool, :tmp_maker, :runner, :exception_class
 
       def initialize(tool: '/usr/bin/unmkinitramfs',
                      tmp_maker: Dir.method(:mktmpdir),
-                     runner: SimpleRunner.new)
+                     runner: Runner::NoResult.new,
+                     exception_class: Runner::ExitError)
         @tool = tool
         @tmp_maker = tmp_maker
         @runner = runner
+        @exception_class = exception_class
       end
     end
   end
--- spec/crypt_reboot/initramfs/extractor_spec.rb
+++ spec/crypt_reboot/initramfs/extractor_spec.rb
@@ -3,14 +3,16 @@
 module CryptReboot
   module Initramfs
     RSpec.describe Extractor do
-      subject(:extractor) { described_class.new(runner: fake_runner) }
+      subject(:extractor) { described_class.new(runner: fake_runner, exception_class: exception_class) }
 
-      def test_file_path(dir)
-        File.join(dir, 'test_file.txt')
-      end
+      let(:exception_class) { ZeroDivisionError }
 
       let :fake_runner do
         ->(_tool, _initramfs, dir) { File.open(test_file_path(dir), 'w') { 0 } }
+      end
+
+      def test_file_path(dir)
+        File.join(dir, 'test_file.txt')
       end
 
       it 'extracts' do
@@ -25,6 +27,18 @@ module CryptReboot
           tmp_dir = dir
         end
         expect(File).not_to exist(tmp_dir)
+      end
+
+      context 'when exception happens' do
+        let :fake_runner do
+          proc { raise exception_class }
+        end
+
+        it 're-raises the exception' do
+          expect do
+            extractor.call('dummy_initramfs') {}
+          end.to raise_error(Extractor::Error)
+        end
       end
     end
   end
