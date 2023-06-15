bbe1d8fb9f5bd693cd51c2a61ad81924d45597c4
Pawel <pepawel@users.noreply.github.com>
2023-06-15 21:29:02 +0200

    Make tmpfs mounting more robust + test

--- lib/crypt_reboot/tmpfs_mounter.rb
+++ lib/crypt_reboot/tmpfs_mounter.rb
@@ -3,19 +3,34 @@
 module CryptReboot
   # Mount tmpfs at the given mount point, yield and unmount
   class TmpfsMounter
-    def call(dir)
-      runner.call('mount', '-t', 'tmpfs', '-o', 'mode=700', 'none', dir)
-      yield
-    ensure
-      runner.call('umount', dir)
+    Error = Class.new StandardError
+
+    def call(dir, &block)
+      mounter.call(dir)
+      run(dir, &block)
+    rescue exception_class => e
+      raise Error, cause: e
     end
 
     private
 
-    attr_reader :runner
+    def run(dir, &block)
+      block.call
+    ensure
+      umounter.call(dir)
+    end
 
-    def initialize(runner: SimpleRunner.new)
+    attr_reader :runner, :mounter, :umounter, :exception_class
+
+    def initialize(verbose: false,
+                   runner: Runner::NoResult.new(verbose: verbose),
+                   mounter: ->(dir) { runner.call('mount', '-t', 'tmpfs', '-o', 'mode=700', 'none', dir) },
+                   umounter: ->(dir) { runner.call('umount', dir) },
+                   exception_class: Runner::ExitError)
       @runner = runner
+      @mounter = mounter
+      @umounter = umounter
+      @exception_class = exception_class
     end
   end
 end
